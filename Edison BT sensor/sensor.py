8ee8367c5a07d13a307ad2c495c8d119f8ad3554
TuZZiX <tianshipei5@126.com>
2017-06-05 08:09:44 -0400

    clean up and add readme

--- Edison BT sensor/sensor.py
+++ Edison BT sensor/sensor.py
@@ -19,59 +19,6 @@ import pyupm_servo
 import pyupm_th02
 import pyupm_i2clcd
 
-
-# try:
-#   from gi.repository import GObject
-# except ImportError:
-#   import gobject as GObject
-
-# class Profile(dbus.service.Object):
-#   fd = -1
-
-#   @dbus.service.method("org.bluez.Profile1",
-#           in_signature="", out_signature="")
-#   def Release(self):
-#     print("Release")
-#     mainloop.quit()
-
-#   @dbus.service.method("org.bluez.Profile1",
-#           in_signature="", out_signature="")
-#   def Cancel(self):
-#     print("Cancel")
-
-#   @dbus.service.method("org.bluez.Profile1",
-#         in_signature="oha{sv}", out_signature="")
-#   def NewConnection(self, path, fd, properties):
-#     self.fd = fd.take()
-#     print("NewConnection(%s, %d)" % (path, self.fd))
-
-
-#     server_sock = socket.fromfd(self.fd, socket.AF_UNIX, socket.SOCK_STREAM)
-#     server_sock.setblocking(1)
-#     server_sock.send("This is Edison SPP loopback test\nAll data will be loopback\nPlease start:\n")
-
-#     try:
-#         while True:
-#             data = server_sock.recv(1024)
-#             print("received: %s" % data)
-#       server_sock.send("looping back: %s\n" % data)
-#     except IOError:
-#         pass
-
-#     server_sock.close()
-#     print("all done")
-
-
-
-#   @dbus.service.method("org.bluez.Profile1",
-#         in_signature="o", out_signature="")
-#   def RequestDisconnection(self, path):
-#     print("RequestDisconnection(%s)" % (path))
-
-#     if (self.fd > 0):
-#       os.close(self.fd)
-#       self.fd = -1
-
 #Analog
 pinSound = 0
 pinMoisture = 1
@@ -155,54 +102,6 @@ def main():
 
 
 if __name__ == '__main__':
-  # dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-  # bus = dbus.SystemBus()
-
-  # manager = dbus.Interface(bus.get_object("org.bluez",
-  #       "/org/bluez"), "org.bluez.ProfileManager1")
-
-  # options.uuid = "1101"
-  # options.psm = "3"
-  # options.role = "server"
-  # options.name = "Edison SPP Loopback"
-  # options.service = "spp char loopback"
-  # options.path = "/foo/bar/profile"
-  # options.auto_connect = False
-  # options.record = ""
-
-  # profile = Profile(bus, options.path)
-
-  # mainloop = GObject.MainLoop()
-
-  # opts = {
-  #     "AutoConnect" : options.auto_connect,
-  #   }
-
-  # if (options.name):
-  #   opts["Name"] = options.name
-
-  # if (options.role):
-  #   opts["Role"] = options.role
-
-  # if (options.psm is not None):
-  #   opts["PSM"] = dbus.UInt16(options.psm)
-
-  # if (options.channel is not None):
-  #   opts["Channel"] = dbus.UInt16(options.channel)
-
-  # if (options.record):
-  #   opts["ServiceRecord"] = options.record
-
-  # if (options.service):
-  #   opts["Service"] = options.service
-
-  # if not options.uuid:
-  #   options.uuid = str(uuid.uuid4())
-
-  # manager.RegisterProfile(options.path, options.uuid, opts)
-
-  # mainloop.run()
   main()
 
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lab03
+
+## Edison BT sensor
+
+Run *sensor.py* to make sure your sensor connection works
+
+Follow commands in *command.txt* to initialize the Bluetooth
+
+Start *BTsensor.py* and run it in background (using screen)
+
+## GoogleMapsTutorial
+
+Run it on your phone, connection to Edison first via upper right corner, then you can start taking pictures. After taken the picture, it will be marked on map with sensor data.
+
+See *Report.pdf*
